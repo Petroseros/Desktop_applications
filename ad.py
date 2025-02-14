f9ed991152660572874bc0fae45330eeda6826f6
Python Course <python@incentar.org>
2025-02-14 19:30:47 +0100

    calc

---
+++ ad.py
@@ -0,0 +1,8 @@
+a = []
+while True:
+    a = [a]
+    print(a)
+
+
+
+
--- code.py
+++ code.py
@@ -1,10 +1,13 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QLineEdit
 
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QLineEdit, QVBoxLayout, QHBoxLayout
+
+a = 0
 class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('hello world')
+        self.setWindowTitle(' ')
         self.create_widget()
 
     def get_click_button(self, n):
@@ -14,15 +17,56 @@ class MyWidget(QWidget):
         return click
 
     def create_widget(self):
-        self.layout = QGridLayout()
+        self.layoutV = QVBoxLayout()
+        self.layoutH123 = QHBoxLayout()
+        self.layoutH456 = QHBoxLayout()
+        self.layoutH789 = QHBoxLayout()
+        self.layoutH0_plus_minus = QHBoxLayout()
+
         self.stroka = QLineEdit()
-        self.layout.addWidget(self.stroka)
+        self.layoutV.addWidget(self.stroka)
+        self.layoutV.addLayout(self.layoutH123)
+        self.layoutV.addLayout(self.layoutH456)
+        self.layoutV.addLayout(self.layoutH789)
+        self.layoutV.addLayout(self.layoutH0_plus_minus)
         self.buttons = []
-        for i in range(9, -1, -1):
+        self.plus_button = QPushButton("+")
+        self.minus_button = QPushButton("-")
+        self.zero_button = QPushButton("0")
+        for i in range(1, 4):
             self.buttons.append(QPushButton(str(i)))
             self.buttons[-1].clicked.connect(self.get_click_button(i))
-            self.layout.addWidget(self.buttons[-1])
-        self.setLayout(self.layout)
+            self.layoutH123.addWidget(self.buttons[-1])
+
+        for i in range(4, 7):
+            self.buttons.append(QPushButton(str(i)))
+            self.buttons[-1].clicked.connect(self.get_click_button(i))
+            self.layoutH456.addWidget(self.buttons[-1])
+
+        for i in range(7, 10):
+            self.buttons.append(QPushButton(str(i)))
+            self.buttons[-1].clicked.connect(self.get_click_button(i))
+            self.layoutH789.addWidget(self.buttons[-1])
+
+        self.plus_button.clicked.connect(self.click_plus_button)
+        self.minus_button.clicked.connect(self.click_minus_button)
+        self.zero_button.clicked.connect(self.click_zero_button)
+        self.h_zero_plus_minus = [self.zero_button, self.plus_button, self.minus_button]
+        for t in range(1,4):
+            self.layoutH0_plus_minus.addWidget(self.h_zero_plus_minus[int(a)])
+            a = a + 1
+
+        self.setLayout(self.layoutV)
+
+    def click_plus_button(self):
+        self.stroka.setText(self.stroka.text() + str("+"))
+
+    def click_minus_button(self):
+        self.stroka.setText(self.stroka.text() + str("-"))
+
+    def click_zero_button(self):
+        self.stroka.setText(self.stroka.text() + str("0"))
+
 
 app = QApplication(sys.argv)
 widget = MyWidget()
